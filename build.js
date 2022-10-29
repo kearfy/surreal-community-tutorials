1b42bf72deb342415b4c7be942657f72aab65f15
Micha de Vries <micha@devrie.sh>
2022-10-29 22:25:02 +0200

    Auto populate guide intro

--- build.js
+++ build.js
@@ -1,11 +1,21 @@
 import fs from 'fs';
 
+const guideIntroTemplate = fs.readFileSync('./theme/guideIntro.html').toString();
+function FillGuideIntro(g) {
+    let tmpl = guideIntroTemplate;
+    tmpl = tmpl.replace('{{TITLE}}', g.title);
+    tmpl = tmpl.replace('{{CREATOR}}', g.creator);
+    tmpl = tmpl.replace('{{LINK}}', g.link);
+    return tmpl;
+}
+
 function CollectFolderGuides(folder) {
     let guides = [];
     const matchOptions = /^---\ntitle: (.*)\ncreator: (.*)\nlink: (.*)\n---$/m;
     const files = fs.readdirSync(`./${folder}/`).filter(f => !f.startsWith('00-'));
     files.forEach(f => {
-        const content = fs.readFileSync(`./${folder}/${f}`).toString();
+        const path = `./${folder}/${f}`;
+        const content = fs.readFileSync(path).toString();
         const matchedOptions = content.match(matchOptions);
         if (!matchedOptions) {
             console.error("Guide has invalid config options");
@@ -13,11 +23,14 @@ function CollectFolderGuides(folder) {
             process.kill(1);
         }
 
-        guides.push({
+        const guide = {
             title: matchedOptions[1],
             creator: matchedOptions[2],
             link: matchedOptions[3]
-        });
+        };
+
+        guides.push(guide);
+        fs.writeFileSync(path, content.replace(matchedOptions[0], FillGuideIntro(guide)))
     });
 
     console.log(JSON.stringify(guides, null, 2));
