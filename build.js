576185a89e4252ebb5076d063e73f27638f966a4
kearfy <kearfy@users.noreply.github.com>
2022-10-30 10:22:53 +0000

    deploy: c8b2b1a23925a49f9fcc70634345e0ff149d6e31

---
+++ build.js
@@ -0,0 +1,55 @@
+import fs from 'fs';
+
+const guideIntroTemplate = fs.readFileSync('./theme/guideIntro.html').toString();
+const rootSummary = {
+    content: fs.readFileSync('./SUMMARY.md').toString(),
+    officialLinks: '- [Introduction](official/00-SUMMARY.md)',
+    communityLinks: '- [Introduction](community/00-SUMMARY.md)'
+};
+
+function FillGuideIntro(g) {
+    let tmpl = guideIntroTemplate;
+    tmpl = tmpl.replace('{{TITLE}}', g.title);
+    tmpl = tmpl.replace('{{CREATOR}}', g.creator);
+    tmpl = tmpl.replace('{{LINK}}', g.link);
+    tmpl = tmpl.replace('{{GH_LINK}}', `https://github.com/surrealdb/tutorials/blob/main/${g.folder}/${g.file}`);
+    return tmpl;
+}
+
+function CollectFolderGuides(folder) {
+    let guides = [];
+    let summaryLinks = "\n";
+    const matchOptions = /^---\ntitle: (.*)\ncreator: (.*)\nlink: (.*)\n---$/m;
+    const files = fs.readdirSync(`./${folder}/`).filter(f => !f.startsWith('00-'));
+
+    files.forEach(f => {
+        const path = `./${folder}/${f}`;
+        const content = fs.readFileSync(path).toString();
+        const matchedOptions = content.match(matchOptions);
+        if (!matchedOptions) {
+            console.error("Guide has invalid config options");
+            console.error(`  - ${f}`);
+            process.kill(1);
+        }
+
+        const guide = {
+            title: matchedOptions[1],
+            creator: matchedOptions[2],
+            link: matchedOptions[3],
+            file: f,
+            folder,
+        };
+
+        fs.writeFileSync(path, content.replace(matchedOptions[0], FillGuideIntro(guide)))
+        summaryLinks += `\n- [${guide.title}](${guide.file}) - [${guide.creator}](${guide.link})`;
+        rootSummary[`${folder}Links`] += `\n    - [${guide.title}](${folder}/${guide.file})`;
+        guides.push(guide);
+    });
+
+    fs.appendFileSync(`./${folder}/00-SUMMARY.md`, summaryLinks);
+    rootSummary.content = rootSummary.content.replace(`- [Introduction](${folder}/00-SUMMARY.md)`, rootSummary[`${folder}Links`]);
+}   
+
+CollectFolderGuides('official');
+CollectFolderGuides('community');
+fs.writeFileSync('./SUMMARY.md', rootSummary.content);
